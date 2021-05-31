11b3e1a35c7114fde6df9628cb1fbaabd712ceb4
prasetyobella17 <danu.17bella@gmail.com>
2021-05-31 19:48:38 +0700

    Repair Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'DashboardController@index');
+Route::get('/', 'DashboardController@index')->name('dashboard');
+
+Route::resource('products', 'ProductController');
 
 Auth::routes(['register' => false]);
